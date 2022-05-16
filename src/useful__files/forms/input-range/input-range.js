0a7f5d0ad67e9c322fc64df7189c4fd329c8561d
Yuri Simonov <y.simonov@alef.im>
2022-05-16 09:07:26 +0300

    шаблон для инпута range

---
+++ src/useful__files/forms/input-range/input-range.js
@@ -0,0 +1,7 @@
+const inputRange = document.querySelector('input[type="range"]');
+const rangePercent = document.querySelector(".range-percent span");
+
+inputRange.addEventListener("input", (event) => {
+	rangePercent.textContent = inputRange.value;
+	inputRange.style.background = `linear-gradient(to right,green 0%,green ${inputRange.value}%,red ${inputRange.value}%,red 100%)`;
+});
